f05d97e0b2bb105938e213ef88b9c5c39d0c6207
btfzddzb <gawdweq@gmail.com>
2025-08-19 23:26:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import os
 import threading
 import uvicorn
 
-from admin import app          # FastAPI
-from bot import dp, executor   # aiogram
+from admin import app
+from bot import dp, executor
 
-def run_bot():
-    executor.start_polling(dp, skip_updates=True)
 
 def run_api():
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)), log_level="info")
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port, log_level="info")
+
 
 if __name__ == "__main__":
-    t = threading.Thread(target=run_bot, daemon=True)
+    # API запускаем фоном
+    t = threading.Thread(target=run_api, daemon=True)
     t.start()
-    run_api()
+
+    # Бота — в главном потоке (так у aiogram есть event loop)
+    executor.start_polling(dp, skip_updates=True)
